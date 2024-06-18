3df6c71e8c2ad418d608953881fe579f8bd33b0f
WesIsCoding <WesCoding232430@gmail.com>
2024-06-18 15:44:42 -0700

    Initial Day 1 Build!

---
+++ src/main/java/wesley/visitingbuildersmod/WessVisitingBuildersClient.java
@@ -0,0 +1,10 @@
+package wesley.visitingbuildersmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WessVisitingBuildersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
